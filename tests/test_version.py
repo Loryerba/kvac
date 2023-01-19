e56b1ae04d3fb333b2baa7fbf3ca6640766bf01b
leonschmidt99 <leon.hand09@gmail.com>
2023-01-19 15:07:42 +0100

    remove import tests

--- tests/test_version.py
+++ tests/test_version.py
@@ -3,12 +3,3 @@ import kvac
 
 def test_version():
     print(kvac.__version__)
-
-
-class TestImports:
-    def test_imports_sho(self):
-        from poksho.poksho import SHO
-
-    def test_imports_curve(self):
-        from curve25519_dalek.ristretto import RistrettoPoint, CompressedRistretto
-        from curve25519_dalek.scalar import Scalar

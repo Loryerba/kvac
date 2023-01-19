b54447ca3f39ad0aff69c161177869cd4d3b9157
leonschmidt99 <leon.hand09@gmail.com>
2023-01-19 23:17:21 +0100

    add test for valid and invalid mac tag

---
+++ tests/test_mac.py
@@ -0,0 +1,26 @@
+import pytest
+
+import kvac
+
+
+class TestMac:
+    @pytest.fixture
+    def macer(self):
+        sho = kvac.RistrettoSho(b'TestMac::macer', b'')
+        system = kvac.SystemParams.generate(4, sho)
+        key = kvac.IssuerKeyPair.generate(system, sho)
+        return kvac.MAC(key)
+
+    def test_mac_valid(self, macer):
+        sho = kvac.RistrettoSho(b'test_mac_valid', b'')
+        messages = [sho.get_point() for _ in range(4)]
+        tag = macer.mac(messages, sho)
+        assert macer.verify(messages, tag)
+
+    def test_mac_invalid(self, macer):
+        sho = kvac.RistrettoSho(b'test_mac_invalid', b'')
+        messages = [sho.get_point() for _ in range(4)]
+        tag = macer.mac(messages, sho)
+
+        messages[0] = sho.get_point()
+        assert not macer.verify(messages, tag)

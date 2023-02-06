3e6b62859c5a4ab2156099ff69b9e979f48c4b84
Konrad H <49533343+konradh@users.noreply.github.com>
2023-02-06 18:06:43 +0100

    Change issuer key pair to not require a SHO as input for randomness
    Also remove redundant value W. It is already given by G_w**w and only
    used in very few places.

--- kvac/issuer_key_pair.py
+++ kvac/issuer_key_pair.py
@@ -1,66 +1,67 @@
+from __future__ import annotations
+import secrets
 from typing import List, NamedTuple
+
 from curve25519_dalek.ristretto import RistrettoPoint
 from curve25519_dalek.scalar import Scalar
 
-from .ristretto_sho import RistrettoSho
-from .system_params import SystemParams
+from kvac.ristretto_sho import RistrettoSho
+from kvac.system_params import SystemParams
 
 
 class IssuerPublicKey(NamedTuple):
-    """
-    Represents the public key of an issuer.
-    """
-    C_w: RistrettoPoint
+    """Represents the public key of an issuer.
+    It also stores global system parameters."""
+    C_W: RistrettoPoint
     I: RistrettoPoint
+    system: SystemParams
+
+    @property
+    def max_attributes(self) -> int:
+        return self.system.max_attributes
+
+
+class IssuerSecretKey(NamedTuple):
+    """Represents the private key of an issuer."""
+    w: Scalar
+    wprime: Scalar
+    x0: Scalar
+    x1: Scalar
+    ys: List[Scalar]
 
 
 class IssuerKeyPair(NamedTuple):
-    """
-    Represents a Server's key pair, including private and public values.
-    """
-    system: SystemParams
+    """Represents an issuer's key pair."""
 
-    # private
-    w: Scalar
-    wprime: Scalar
-    W: RistrettoPoint
-
-    x0: Scalar
-    x1: Scalar
-
-    ys: List[Scalar]
-
-    # public
-    C_w: RistrettoPoint
-    I: RistrettoPoint
+    secret: IssuerSecretKey
+    public: IssuerPublicKey
 
     @classmethod
     def generate(
             cls,
             system: SystemParams,
-            sho: RistrettoSho
-    ) -> 'IssuerKeyPair':
+    ) -> IssuerKeyPair:
+        """Generates a new issuer key pair."""
+        sho = RistrettoSho(
+            b'kvac.issuer_key_pair.IssuerKeyPair.generate',
+            secrets.token_bytes(256))
 
-        # private
+        # private values
         w = sho.get_scalar()
         wprime = sho.get_scalar()
-        W = system.G_w * w
         x0 = sho.get_scalar()
         x1 = sho.get_scalar()
-
         ys = [sho.get_scalar() for _ in range(system.max_attributes)]
 
-        # public
-        C_w = W + (system.G_wprime * wprime)
+        C_W = (system.G_w * w) + (system.G_wprime * wprime)
         I = system.G_V - (system.G_x0 * x0) - (system.G_x1 * x1)
         for G_y, y in zip(system.G_ys, ys):
             I -= G_y * y
 
-        return cls(system, w, wprime, W, x0, x1, ys, C_w, I)
-
-    def get_public_key(self) -> IssuerPublicKey:
-        return IssuerPublicKey(self.C_w, self.I)
+        secret = IssuerSecretKey(w=w, wprime=wprime, x0=x0, x1=x1, ys=ys)
+        public = IssuerPublicKey(C_W=C_W, I=I, system=system)
+        return cls(secret=secret, public=public)
 
     @property
     def max_attributes(self) -> int:
-        return self.system.max_attributes
+        return self.public.max_attributes
